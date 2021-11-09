c3ca6857e32c51ba6db2347e4c97cfa5727fc6fe
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-11-09 10:08:47 -0500

    first commit

---
+++ src/main/java/Tables.java
@@ -0,0 +1,13 @@
+public class Tables {
+    public String getSmallMultiplicationTable() {
+        return null;
+    }
+
+    public String getLargeMultiplicationTable() {
+        return null;
+    }
+
+    public String getMultiplicationTable(int tableSize) {
+        return null;
+    }
+}
