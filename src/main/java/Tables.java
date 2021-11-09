d680acb08736852e8f5e3f264fa005920fa1e7e3
DeShawn Wallace <deshawnwallace@DeShawns-MacBook-Air.local>
2021-11-09 13:36:14 -0500

    Completed 03

--- src/main/java/Tables.java
+++ src/main/java/Tables.java
@@ -1,13 +1,51 @@
 public class Tables {
     public String getSmallMultiplicationTable() {
-        return null;
+
+        StringBuilder row = new StringBuilder();
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                int number = i * j;
+                row.append(String.format("%5s", number + " |"));
+            }
+
+            row.append(System.lineSeparator());
+        }
+
+        return row.toString();
+
     }
 
     public String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder row = new StringBuilder();
+
+        for (int i = 1; i <= 10; i++) {
+
+            for (int j = 1; j <= 10; j++) {
+                int number = i * j;
+                row.append(String.format("%5s", number + " |"));
+            }
+
+            row.append(System.lineSeparator());
+        }
+
+        return row.toString();
     }
 
     public String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder row = new StringBuilder();
+
+        for (int i = 1; i <= 20; i++) {
+
+            for (int j = 1; j <= 20; j++) {
+                int number = i * j;
+                row.append(String.format("%5s", number + " |"));
+            }
+
+            row.append(System.lineSeparator());
+        }
+
+        return row.toString();
     }
 }
